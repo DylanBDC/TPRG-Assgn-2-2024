1627cdf309adf4aaeffaa0e53f81d08576ea9528
DylanBDC <dylan.brett@dcmail.ca>
2024-11-30 18:49:37 -0500

    Working server
    In this commit, I got my client to work with my server. Next, I will update my client to get real-time values of the data and add the three other arguments

--- Client/Client_vcgencmds_Dylan Brett.py
+++ Client/Client_vcgencmds_Dylan Brett.py
@@ -1,23 +1,38 @@
 # Dylan Brett (100933134)
 # TPRG-2131-02
-# Nov 25, 2024
+# Nov 30, 2024
 # This program is strictly my own work. Any material
 # beyond course learning materials that is taken from
 # the Web or other sources is properly cited, giving
 # credit to the original author(s). I havent used any
 # code from other sources other than referncing the course material
 
-import socket
+#Cited
+#https://www.datacamp.com/tutorial/json-data-python?utm_source=google&utm_medium=paid_search
 
-Runs on PC, directly from Thonny
-The client
+import socket
+import json
+
+# Runs on PC, directly from Thonny
+# The client
 print("The client")
 
-for x in range(6):
-    s = socket.socket()
-    host = '192.168.10.111'  # ip of raspberry PI, running the server
-    port = 5000
-    s.connect((host, port))
-    print(s.recv(1024))
-    s.close()
+
+s = socket.socket()
+host = '10.0.0.178'  # ip of raspberry PI, running the server
+port = 5000
+s.connect((host, port))
+
+
+encoded_string = s.recv(1024)
+#data = encoded_string.decode('utf-8')
+data = json.loads(encoded_string) # json.loads decodes the JSON string into a python object
+core = data['Temperature'] # access the temp using the temperature key in the data dictionarry 
+volts = data['Voltage'] # access voltage in data
+print(encoded_string)
+print("Core Temperature:", core)
+print("Core Voltage", volts)
+        
+
+s.close()
     
