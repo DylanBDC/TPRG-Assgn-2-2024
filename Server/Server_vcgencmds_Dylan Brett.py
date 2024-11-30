93fd5259fb38df4fe549ff01b4177752f5bb5361
DylanBDC <dylan.brett@dcmail.ca>
2024-11-30 18:23:55 -0500

    Added Temp and Volts readings
    In this commit I have added the Temp and Voltage readings of the RPI. I have added them using the JSON client and JSON server files we looked at in class (I hope I am doing this correctly for the assignment) also the links I have posted. I have done this by turning the python object into a JSON string using json.dumps and sending the encoded string to the client code.

--- Server/Server_vcgencmds_Dylan Brett.py
+++ Server/Server_vcgencmds_Dylan Brett.py
@@ -1,6 +1,6 @@
 # Dylan Brett (100933134)
 # TPRG-2131-02
-# Nov 25, 2024
+# Nov 30, 2024
 # This program is strictly my own work. Any material
 # beyond course learning materials that is taken from
 # the Web or other sources is properly cited, giving
@@ -19,25 +19,48 @@ import os, time
 import json
 
 s = socket.socket()
-host = '' # Localhost
+host = '10.0.0.178' # Localhost
 port = 5000
 s.bind((host, port))
 s.listen(5)
 
+def RPi_temp():
+    #gets the Core Temperature from Pi, ref https://github.com/nicmcd/vcgencmd/blob/master/README.md
+    #t = os.popen('vcgencmd measure_volts ain1').readline() #gets from the os, using vcgencmd - the core-temperature
+    core = os.popen('vcgencmd measure_temp').readline()
+    # initialising json object string
+    #ini_string = {"Temperature": core}
+    #ini_string = json.dumps(ini_string)
+    #jsonbyte = bytes(ini_string, "UTF-8")
+    # converting string to json
+    #f_dict = eval(ini_string) # The eval() function evaluates JavaScript code represented as a string and returns its completion value.
+    #return f_dict
+    return core
 
-#gets the Core Temperature from Pi, ref https://github.com/nicmcd/vcgencmd/blob/master/README.md
-t = os.popen('vcgencmd measure_volts ain1').readline() #gets from the os, using vcgencmd - the core-temperature
-# initialising json object string
-ini_string = """{"Temperature": t}"""
-# converting string to json
-f_dict = eval(ini_string) # The eval() function evaluates JavaScript code represented as a string and returns its completion value.
+def volts():
+    volts = os.popen('vcgencmd measure_volts core').readline()
+    
+    #voltage = {"Core Voltage": core}
+    #voltage = json.dumps(voltage)
+    #jsonbytevolt = bytes(voltage, "UTF-8")
+    
+    return volts
+    
+    
 
 
 
 while True:
-  c, addr = s.accept()
-  print ('Got connection from',addr)
-  res = bytes(str(f_dict), 'utf-8') # needs to be a byte
-  c.send(res) # sends data as a byte type
-  c.close()
+    c, addr = s.accept()
+    print ('Got connection from',addr)
+    core = RPi_temp()
+    volts = volts()
+  
+    jsonResult = {"Temperature": core, "Voltage": volts}
+    jsonResult = json.dumps(jsonResult)
+    jsonbyte = bytearray(jsonResult, "UTF-8")
+    #res = bytes(str(RPi_temp()), 'utf-8') # needs to be a byte
+    c.send(jsonbyte) # sends data as a byte type
+    #c.send(volts())
+    c.close()
 
